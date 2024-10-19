0e472336373507d606fa5bfcb995807439102b12
Sagar-rm <sagarrathod292007@gmail.com>
2024-10-19 22:06:16 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={reactLogo} className="logo react" alt="React logo" />
 
-        <h1>Hello from Amplify</h1>
-      </header>
+      
+       <header className="App-header">
+         <img src={reactLogo} className="logo react" alt="React logo" />
+
+         <h1>Hello from Amplify adfasfass</h1>
+     </header>
     </div>
   );
 }
---
+++ src/component/add.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Add(){
+    let num1 = 5;
+    let num2 = 2;
+    let sum = num1 + num2;
+return 
+    <>
+    <h1>{sum}</h1>
+    </>
+}
